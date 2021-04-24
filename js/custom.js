85da4381639eba1edea5badebdf5a51f9553a466
Chloe C <chungchloe@pm.me>
2021-04-24 15:53:13 -0400

    Added finished files

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+document.getElementById("red-button").addEventListener("click", function() {
+    document.getElementById("red").scrollIntoView();
+});
+
+document.getElementById("green-button").addEventListener("click", function() {
+    document.getElementById("green").scrollIntoView();
+});
+
+document.getElementById("navy-button").addEventListener("click", function() {
+    document.getElementById("navy").scrollIntoView();
+});
