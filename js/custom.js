9f0f0c7721a410367355fc7b75a1a6d3c9612c96
Chloe C <chungchloe@pm.me>
2021-04-26 12:38:03 -0400

    Added comments and new solutions

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+//1. First Solution - addEventListner
+//Benefits: Easty to write, no modification to html necessary
+//Limitations: Manual. Same lines of code have to be duplicated for the function to be applied to a new image.
 /*document.getElementById("red-button").addEventListener("click", function() {
     document.getElementById("red").scrollIntoView();
 });
@@ -12,23 +15,26 @@ document.getElementById("navy-button").addEventListener("click", function() {
 
 
 
+//2. Second Solution(?) - addEventListner 2
 /*var colourButtons = document.querySelectorAll(".colour-button").length;
 var myId = null;
 
 for (var i=0; i<colourButtons; i++) { 
     myId = document.querySelectorAll(".product-img")[i].id;
-    console.log("myId = (" + myId+ "), myId = (" + myId+ ")"); 
     document.querySelectorAll(".colour-button")[i].addEventListener("click", function() {
         document.getElementById(myId).scrollIntoView();
     });
 }*/
 
 
-function scrollToImg(buttonId) {
-    //console.log(buttonId);
+
+//3. Third Solution - Switch statement
+//Benefits: All functions are combined into one.
+//Limitations: HTML has to be modified, a new switch statement has to be added whenever there is a new image.
+/*function scrollToImg(buttonId) {
     var productImgId = null;
     
-    /*switch (myId) {
+    switch (myId) {
         case "green-button" :
         myImgId = "green";
         break;
@@ -40,9 +46,26 @@ function scrollToImg(buttonId) {
         case "navy-button" :
         myImgId = "navy";
         break;
-    }*/
-
-    productImgId = buttonId.substring(0, buttonId.indexOf('-'));
+    }
     
     document.getElementById(productImgId).scrollIntoView();
+}*/
+
+
+
+//4. Fourth Solution - substring method
+//Benefits: Simple code
+//Limitations: HTML has to be modified. Image ids and button ids have to strictly follow a certain naming convention.
+function scrollToImg(buttonId) {
+    var productImgId = null;
+
+    productImgId = buttonId.substring(0, buttonId.indexOf('-'));
+
+    document.getElementById(productImgId).scrollIntoView();
 }
+
+
+
+//5. Fifth Solution - Anchor tag
+//Benefits: Easy to write, accessible.
+//Limitations: Image ids cannot be changed.
