9b96ca56b155c46d7fd7e6d2741ad6e8413add0d
Chloe C <chungchloe@pm.me>
2021-04-25 20:44:44 -0400

    Combined functions into one

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-document.getElementById("red-button").addEventListener("click", function() {
+/*document.getElementById("red-button").addEventListener("click", function() {
     document.getElementById("red").scrollIntoView();
 });
 
@@ -8,4 +8,39 @@ document.getElementById("green-button").addEventListener("click", function() {
 
 document.getElementById("navy-button").addEventListener("click", function() {
     document.getElementById("navy").scrollIntoView();
-});
+});*/
+
+
+
+/*var colourButtons = document.querySelectorAll(".colour-button").length;
+var myId = null;
+
+for (var i=0; i<colourButtons; i++) { 
+    myId = document.querySelectorAll(".product-img")[i].id;
+    console.log("myId = (" + myId+ "), myId = (" + myId+ ")"); 
+    document.querySelectorAll(".colour-button")[i].addEventListener("click", function() {
+        document.getElementById(myId).scrollIntoView();
+    });
+}*/
+
+
+function scrollToImg(myId) {
+    console.log(myId);
+    var myImgId = null;
+    
+    switch (myId) {
+        case "green-button" :
+        myImgId = "green";
+        break;
+
+        case "red-button" :
+        myImgId = "red";
+        break;
+
+        case "navy-button" :
+        myImgId = "navy";
+        break;
+    }
+
+    document.getElementById(myImgId).scrollIntoView();
+}
